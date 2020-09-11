a994d2915a14a09bd22b77334e672c50e7495f2d
Mateusz Zakrzewski <mateusz.zakrzewski@meelogic.com>
2020-09-11 21:44:56 +0200

    Code formatting

--- src/distanceUtils.ts
+++ src/distanceUtils.ts
@@ -1,27 +1,26 @@
 function degreesToRadians(degrees: number): number {
-    return degrees * Math.PI / 180;
+  return (degrees * Math.PI) / 180;
 }
 
-export function getDistanceBetweenPoints(lat1: number, lng1: number, lat2: number, lng2: number): number {
-    // The radius of the planet earth in meters
-    let R = 6378137;
-    let dLat = degreesToRadians(lat2 - lat1);
-    let dLong = degreesToRadians(lng2 - lng1);
-    let a = Math.sin(dLat / 2)
-            *
-            Math.sin(dLat / 2) 
-            +
-            Math.cos(degreesToRadians(lat1)) 
-            * 
-            Math.cos(degreesToRadians(lat1)) 
-            *
-            Math.sin(dLong / 2) 
-            * 
-            Math.sin(dLong / 2);
+export function getDistanceBetweenPoints(
+  lat1: number,
+  lng1: number,
+  lat2: number,
+  lng2: number
+): number {
+  // The radius of the planet earth in meters
+  let R = 6378137;
+  let dLat = degreesToRadians(lat2 - lat1);
+  let dLong = degreesToRadians(lng2 - lng1);
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degreesToRadians(lat1)) *
+      Math.cos(degreesToRadians(lat1)) *
+      Math.sin(dLong / 2) *
+      Math.sin(dLong / 2);
 
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let distance = R * c;
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let distance = R * c;
 
-    return distance;
+  return distance;
 }
-
